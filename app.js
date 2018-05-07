ee304f5a431dc20bd8f0d563c7c3ebe21399fb9b
PravinKumar <pravinkumar@classicinformatics.com>
2018-05-07 17:10:58 +0530

    correct spells

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const sass = require('node-sass-middleware');
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
 dotenv.load({ path: '.env' });
-console.log(process.env);
 /**
  * Controllers (route handlers).
  */
--- controllers/home.js
+++ controllers/home.js
@@ -4,7 +4,6 @@
  */
 var rates =  require('../models/rates')
 exports.index = (req, res) => {
-  console.log(req.body)
   res.render('home', {
     title: 'HOME',
     rates: rates
